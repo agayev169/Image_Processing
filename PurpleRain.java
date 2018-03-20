393e0603ce1fc5f4591f4ac0d1e92bc96974adde
Kamal Aghayev <34622021+agayev169@users.noreply.github.com>
2018-03-20 14:09:02 +0400

    Add files via upload

---
+++ PurpleRain.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Line2D;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class PurpleRain extends JPanel implements ActionListener {
+
+    static final int WIDTH = 640, HEIGHT = 480;
+    static final float MAX_VEL = 5.0f, MIN_VEL = 1.0f;
+
+    static class Rain {
+        int x, y1, y2;
+        float vel;
+        Rain() {
+            Random rand = new Random();
+            x = rand.nextInt(WIDTH);
+            y1 = rand.nextInt(120) - 200;
+            y2 = y1 + 20 + rand.nextInt(30);
+            vel = (y2 - y1) / 50.0f * (MAX_VEL - MIN_VEL) + MIN_VEL;
+        }
+    }
+
+    static LinkedList<Rain> lines = new LinkedList<>();
+    Timer t = new Timer(5, this);
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        PurpleRain jp = new PurpleRain();
+        lines.add(new Rain());
+        jf.setSize(WIDTH, HEIGHT);
+        jf.setLayout(new BorderLayout());
+        jf.setTitle("Purple Rain");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+        jf.add(jp);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        t.start();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        for (Rain l : lines) {
+            g.setColor(new Color(200 + (int)(l.vel / MAX_VEL * 55), 0,
+                    200 + (int)(l.vel / MAX_VEL * 55)));
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(l.vel / 2));
+            g2.draw(new Line2D.Float(l.x, l.y1, l.x, l.y2));
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        lines.add(new Rain());
+        lines.add(new Rain());
+        for (Rain l : lines) {
+            l.y1 += l.vel;
+            l.y2 += l.vel;
+        }
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).y1 > 500) lines.remove(i);
+        }
+
+        repaint();
+    }
+}
